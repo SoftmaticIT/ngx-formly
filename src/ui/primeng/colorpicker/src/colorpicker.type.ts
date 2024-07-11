4d3f1b610316035e7bad7ee598252b7051704b4b
Domenico <domenico.migliaccio@softmatic.it>
2024-07-11 10:08:30 +0200

    update

--- src/ui/primeng/colorpicker/src/colorpicker.type.ts
+++ src/ui/primeng/colorpicker/src/colorpicker.type.ts
@@ -2,7 +2,6 @@ import { Component, ChangeDetectionStrategy, Type } from '@angular/core';
 import { FieldType, FieldTypeConfig, FormlyFieldConfig } from '@ngx-formly/core';
 import { FormlyFieldProps } from '@ngx-formly/primeng/form-field';
 import { ColorPickerChangeEvent } from 'primeng/colorpicker';
-import { Subject, takeUntil } from 'rxjs';
 
 interface ColorPickerProps extends FormlyFieldProps {
   inputStyle?: string;
@@ -18,7 +17,7 @@ interface ColorPickerProps extends FormlyFieldProps {
   showTransitionOptions?: string;
   hideTransitionOptions?: string;
   autofocus?: boolean;
-  onChange?: (currentValue: string) => void;
+  onChange?: (event: ColorPickerChangeEvent) => void;
   onShow?: (event: any) => void;
   onHide?: (event: any) => void;
 }
@@ -32,50 +31,4 @@ export interface FormlyColorPickerFieldConfig extends FormlyFieldConfig<ColorPic
   templateUrl: './colorpicker.type.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormlyFieldColorPicker extends FieldType<FieldTypeConfig<ColorPickerProps>> {
-  private readonly _destroy$: Subject<void> = new Subject<void>();
-
-  ngOnInit(): void {
-    if (!this.formControl.value) {
-      this.formControl.setValue('#ffffff');
-    }
-
-    this.formControl.valueChanges.pipe(takeUntil(this._destroy$)).subscribe((value) => {
-      if (!value) {
-        this.formControl.setValue('#ffffff');
-      } else if (this.props.onChange) {
-        this.props.onChange(value);
-      }
-    });
-  }
-
-  onShow(event: any): void {
-    if (this.props.onShow) {
-      this.props.onShow(event);
-    }
-  }
-
-  onHide(event: any): void {
-    if (this.props.onShow) {
-      this.props.onHide(event);
-    }
-  }
-
-  ngOnDestroy(): void {
-    this._destroy$.next();
-    this._destroy$.complete();
-  }
-
-  handleColorChange(currentValue: string): void {
-    this.formControl.setValue(currentValue);
-  }
-
-  handleBlur(event: any): void {
-    const { value } = event.target;
-    if (!value || value === '') {
-      this.formControl.setValue('#ffffff');
-    } else if (!(value as string).includes('#')) {
-      this.formControl.setValue(`#${value}`);
-    }
-  }
-}
+export class FormlyFieldColorPicker extends FieldType<FieldTypeConfig<ColorPickerProps>> {}
